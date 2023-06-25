a1812caffd86602f1064867ec9269072acc14f55
kanosy88 <billyagbo38@gmail.com>
2023-06-25 06:28:02 +0200

    Rangement

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,6 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import {
-  Button,
-  Container,
-  Divider,
-  HStack,
-  Heading,
-  Input,
-  Text,
-  Textarea,
-  useToast,
-} from "@chakra-ui/react";
+import { Container, Divider, Heading, useToast } from "@chakra-ui/react";
 import FlashCard from "./compenents/FlashCardCreator";
 import IdComp from "./compenents/IdInput";
 import MultiFlashCardCreator from "./compenents/MulitFlashCardCreator";
